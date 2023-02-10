97465955cc26017b76e2159edadc44782e61b58a
Alex Sabani <47181191+alexander1220@users.noreply.github.com>
2023-02-10 21:57:57 +0100

    Projektdateien hinzufügen.

---
+++ ConsoleDrawingLibrary/ConsoleDrawer.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDrawingLibrary
+{
+    public class ConsoleDrawer
+    {
+        public static void DrawPoint(int x, int y, ConsoleColor color = ConsoleColor.White, ConsoleColor backgroundColor = ConsoleColor.Black, char symbol = '#')
+        {
+            if (x < 0) x = 0;
+            if (y < 0) y = 0;
+            if (x >= Console.WindowWidth) return;
+            if (y >= Console.WindowHeight) return;
+
+            var curColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.SetCursorPosition(x, y);
+            Console.Write(symbol);
+            Console.ForegroundColor = curColor;
+        }
+
+        public static void DrawLine(int x1, int y1, int x2, int y2, ConsoleColor color = ConsoleColor.White, ConsoleColor backgroundColor = ConsoleColor.Black, char symbol = '#')
+        {
+            int xDif = x2 - x1;
+            int yDif = y2 - y1;
+            var length = Math.Sqrt(yDif * yDif + xDif * xDif);
+            for (int i = 0; i < length; i++)
+            {
+                if (i == 28)
+                {
+                    Console.Write("");
+                }
+                var mappedX = Math.Round((i / length) * xDif);
+                var mappedY = Math.Round((i / length) * yDif);
+                DrawPoint(x1 + (int)mappedX, y1 + (int)mappedY, color, backgroundColor, symbol);
+            }
+            DrawPoint(x1, y1, color, backgroundColor, symbol);
+            DrawPoint(x2, y2, color, backgroundColor, symbol);
+        }
+
+        public static void DrawRect(int x, int y, int width, int height, ConsoleColor color = ConsoleColor.White, ConsoleColor backgroundColor = ConsoleColor.Black, char symbol = '#')
+        {
+            DrawLine(x, y, x + width, y, color, backgroundColor, symbol);
+            DrawLine(x, y + height, x + width, y + height, color, backgroundColor, symbol);
+            DrawLine(x, y, x, y + height, color, backgroundColor, symbol);
+            DrawLine(x + width, y, x + width, y + height, color, backgroundColor, symbol);
+        }
+        public static void DrawFilledRect(int x, int y, int width, int height, ConsoleColor color = ConsoleColor.White, ConsoleColor backgroundColor = ConsoleColor.Black, char symbol = '#')
+        {
+            if (height < width)
+                for (int i = 0; i < height; i++)
+                {
+                    DrawLine(x, y + i, x + width, y + i, color, backgroundColor, symbol);
+                }
+            else
+                for (int i = 0; i < width; i++)
+                {
+                    DrawLine(x + i, y, x + i, y + height, color, backgroundColor, symbol);
+                }
+        }
+
+        public static void DrawBezier(int startX, int startY, int endX, int endY, int curveX, int curveY, ConsoleColor color = ConsoleColor.White, ConsoleColor backgroundColor = ConsoleColor.Black, char symbol = '#')
+        {
+            (int x, int y) start = (startX, startY);
+            (int x, int y) end = (endX, endY);
+            (int x, int y) curve = (curveX, curveY);
+            int len = 100;
+            for (float i = 0; i < len; i++)
+            {
+                float iter = i / len;
+                var x1 = Lerp(start.x, curve.x, iter);
+                var y1 = Lerp(start.y, curve.y, iter);
+                var x2 = Lerp(curve.x, end.x, iter);
+                var y2 = Lerp(curve.y, end.y, iter);
+
+                var newX = Lerp(x1, x2, iter);
+                var newY = Lerp(y1, y2, iter);
+                DrawPoint((int)newX, (int)newY, color, backgroundColor, symbol);
+            }
+            //"Handles"
+            //DrawLine(start.x,start.y,curve.x,curve.y,ConsoleColor.Blue);
+            //DrawLine(end.x,end.y,curve.x,curve.y, ConsoleColor.Red);
+        }
+
+        public static void DrawCircle(int x, int y, int radius, ConsoleColor color = ConsoleColor.White, ConsoleColor backgroundColor = ConsoleColor.Black, char symbol = '#')
+        {
+            float len = radius * 2 * 3.2f;
+            for (int i = 0; i < radius * 2 + 1; i++)
+            {
+                int cur = i;
+                int toMid = radius - cur;
+                int circleY = (int)Math.Sqrt(radius * radius - toMid * toMid);
+                DrawPoint(x + i, y + circleY, color, backgroundColor, symbol);
+                DrawPoint(x + i, y - circleY, color, backgroundColor, symbol);
+
+                DrawPoint(x + radius + circleY, y - radius + i, color, backgroundColor, symbol);
+                DrawPoint(x + radius - circleY, y - radius + i, color, backgroundColor, symbol);
+            }
+        }
+
+        public static void DrawFilledCircle(int x, int y, int radius, ConsoleColor color = ConsoleColor.White, ConsoleColor backgroundColor = ConsoleColor.Black, char symbol = '#')
+        {
+            float len = radius * 2 * 3.2f;
+            for (int i = 0; i < radius * 2 + 1; i++)
+            {
+                int cur = i;
+                int toMid = radius - cur;
+                int circleY = (int)Math.Sqrt(radius * radius - toMid * toMid);
+                DrawLine(x + i, y, x + i, y + circleY, color, backgroundColor, symbol);
+                DrawLine(x + i, y, x + i, y - circleY, color, backgroundColor, symbol);
+            }
+        }
+
+        static float Lerp(float firstFloat, float secondFloat, float by)
+        {
+            return firstFloat * (1 - by) + secondFloat * by;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,50 @@
+![Banner](https://user-images.githubusercontent.com/47181191/218121404-1d86b636-49a9-414f-81c7-7885038e15ba.png)
+
+
+# Console Drawer Library
+
+ [![C#](https://img.shields.io/badge/Language-C%23-blue?style=for-the-badge&logo=.net)](https://en.wikipedia.org/wiki/C_Sharp_(programming_language)) 
+
+## :open_book: Project Overview :
+
+This is a DLL written in C#, it uses the most basic methods to **draw simple, colored forms** in the console.
+
+It can be used to quickly draw simple forms, when creating e.g. user interfaces.
+
+You can draw:
+
+ - Points
+ - Lines
+ - Rectangles
+ - Circles
+ - Bezier Curves
+
+## 🚀 Getting Started :
+
+### Visual Studio :
+
+1. Right-click on your project
+2. Select Add > Project Reference
+3. Click "Browse" > select the .dll > click "Ok"
+3. Done 🎉
+
+## 🧪 Usage :
+
+### How to use the library :
+
+**Import it:**
+
+    using ConsoleDrawingLibrary;
+
+**Use it:**
+
+    ConsoleDrawer.DrawPoint(10, 10);
+
+
+## 💡 Tip:
+
+Set your console font (Right click on the window > Preferences > Font) to "Raster Fonts" and use a quadratic size (e.g. 8x8) to prevent distortion
+
+### Demonstration :
+
+VIDEO
